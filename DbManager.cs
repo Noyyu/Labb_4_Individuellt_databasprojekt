d9c7466f34e286cba96dd55702261c0cbdefc647
Noyyu <Nikki.Norberg@hotmail.com>
2026-01-28 18:10:01 +0100

    Added scaffolding to Labb 3

---
+++ DbManager.cs
@@ -0,0 +1,88 @@
+﻿using SQLScaffolding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace SQLScaffolding
+{
+    internal class DbManager
+    {
+        public static void AddStaff(string name, int titleId)
+        {
+            using (var context = new SchoolContext())
+            {
+                var newStaff = new Staff { Name = name, TitleId = titleId };
+                context.Staff.Add(newStaff);
+                context.SaveChanges();
+                Console.WriteLine($"{newStaff.Name} has been added to the Staff Table");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue..");
+            Console.ReadKey();
+        }
+        public static void PrintClass(int classId)
+        {
+            using (var context = new SchoolContext())
+            {
+                var classToPrint = context.Classes.FirstOrDefault(className  => className.Id == classId);
+                var studentsInClass = context.Students.ToList();
+                var studentClass = context.Students.ToList();
+
+                if (classToPrint != null)
+                {
+                    Console.WriteLine($"{"NAME",-15} {"LAST NAME",-15} {"CLASS",-2}");
+                    Console.WriteLine(new string('-', 35));
+
+                    foreach (var student in studentsInClass)
+                    {
+                        if (student.ClassId == classId)
+                        {
+                            Console.WriteLine($"{student.Name, -15} {student.LastName, -15} - {student.Class.Name, -2}");
+                        }
+                    }
+                }
+                else { Console.WriteLine("This class does not exsist"); }              
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue..");
+            Console.ReadKey();
+        }
+        public static void PrintStudents(bool desc, bool name)
+        {
+            using (var context = new SchoolContext())
+            {
+                var query = context.Students.Include(student => student.Class).AsQueryable();
+                var students = query.ToList();
+                var studentClass = context.Classes.ToList();
+
+                if (name)
+                {
+                    query = desc ? query.OrderByDescending(sort => sort.Name)
+                                 : query.OrderBy(sort => sort.Name);
+                }
+                else
+                {
+                    query = desc ? query.OrderByDescending(sort => sort.LastName)
+                                 : query.OrderBy(sort => sort.LastName);
+                }             
+
+                Console.WriteLine($"{"NAME",-15} {"LAST NAME",-15} {"CLASS", -2}");
+                Console.WriteLine(new string('-', 35));
+
+                foreach ( var student in students)
+                {
+                    Console.WriteLine($"{student.Name, - 15} {student.LastName, - 15} - {student.Class.Name, -2}");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Press any key to continue..");
+                Console.ReadKey();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,89 @@
-﻿namespace SQLScaffolding
+﻿using Microsoft.Data.SqlClient;
+using SQLScaffolding;
+
+namespace Labb_3___Anropa_databasen
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Menu();
         }
+
+        static void Menu()
+        {
+            int exit = -1;
+            while (exit != 0)
+            {
+                Console.Clear();
+                Console.WriteLine("1: See students");
+                Console.WriteLine("2: See classes");
+                Console.WriteLine("3: Add staff");
+                Console.WriteLine("0: Exit");
+
+                switch (UserNumberChoise(0, 3))
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("1: Sort by last name");
+                        Console.WriteLine("2: Sort by first name");
+                        bool userBoolOne = UserNumberChoise(1, 2) == 2;
+                        Console.Clear();
+
+                        Console.WriteLine("1: Sort by decending order");
+                        Console.WriteLine("2: Sort by acanding order");
+                        bool userBoolTwo = UserNumberChoise(1, 2) == 1;
+                        Console.Clear();
+
+                        DbManager.PrintStudents(userBoolTwo, userBoolOne);
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("1: Class 1A");
+                        Console.WriteLine("2: Class 2B");
+                        Console.WriteLine("3: Class 3C");
+                        Console.WriteLine("4: Class 4D");
+                        Console.WriteLine("5: Class 5A");
+                        Console.WriteLine();
+
+                        DbManager.PrintClass(UserNumberChoise(1, 5));
+                        break;
+
+                    case 3:
+                        string name;
+
+                        Console.Clear();
+                        Console.WriteLine("Staff name?");
+                        name = Console.ReadLine();
+                        Console.Clear();
+
+                        Console.WriteLine("Staff title?");
+                        Console.WriteLine("1: Admin");
+                        Console.WriteLine("2: Rektor");
+                        Console.WriteLine("3: Lärare");
+                        Console.WriteLine("4: IT");
+                        Console.WriteLine("5: Vaktmästare");
+                        Console.WriteLine();
+
+                        DbManager.AddStaff(name, UserNumberChoise(1, 5));
+                        break;
+                    case 0:
+                        exit = 0;
+                        break;
+                }
+            }
+
+            static int UserNumberChoise(int min, int max)
+            {
+                int userChoise = -1;
+                while (!int.TryParse(Console.ReadLine(), out userChoise) || userChoise > max || userChoise < min)
+                {
+                    Console.WriteLine("Invalid choice");
+                }
+                return userChoise;
+            }
+        }     
     }
 }
+
